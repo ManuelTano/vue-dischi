da536bf9383aeaff0c612a30342602792026f739
ManuelTano <manueltano98@gmail.com>
2022-07-26 18:20:16 +0200

    completed header

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="hello">
     <header>
-      <div class="container h-100">
+      <div class="px-3 h-100 d-flex align-items-center">
         <img src="../assets/spotify.png" alt="Spotify Logo" />
       </div>
     </header>
-  </div>
 </template>
 
 <script>
@@ -15,10 +13,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/scss/vars";
+
 header {
+  background-color: $primary-color;
   height: 100px;
 }
 img {
-  height: 80%;
+  height: 50%;
 }
 </style> 
